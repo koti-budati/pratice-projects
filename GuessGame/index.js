5ec94f9886668a523794fc53102a44b68f63db9e
koti-budati <koti.budati1994@outlook.com>
2021-08-12 20:38:25 +0530

    guessing game almost complete

--- GuessGame/index.js
+++ GuessGame/index.js
@@ -1,10 +1,38 @@
+"use strict";
+
 const start_game = document.getElementById("start-game");
+const game_on = document.getElementById("game-on");
+const submit_number = document.getElementById("submit-number");
+const selected_number = document.getElementById("selected-number");
+const comment = document.getElementById("comment");
 
-const func = (event) => {
-  alert("click");
-  if (event.error) {
-    alert(event.error);
+var randomNumber;
+var count = 0;
+var userEntryList = [];
+
+start_game.addEventListener("click", () => {
+  randomNumber = Math.floor(Math.random() * 100 + 1);
+  start_game.style.display = "none";
+  game_on.style.display = "inline";
+});
+
+submit_number.addEventListener("click", () => {
+  var userValue = selected_number.value;
+  userEntryList.push(userValue);
+  comment.innerText = `You Have Entered: ${userEntryList}`;
+  if (count >= 9) {
+    comment.innerText = `You Have Entered: ${userValue}`;
+  } else if (userValue > 100) {
+    comment.innerText = `please enter a value between 1 - 100\nYou Have Entered: ${userEntryList}`;
+  } else if (userValue == randomNumber) {
+    comment.innerText = `Congrats You Win\nYou Have Entered: ${userEntryList}`;
+    start_game.style.display = "block";
+    submit_number.disabled = true;
+  } else if (userValue > randomNumber) {
+    comment.innerText = `Way too High\nYou Have Entered: ${userEntryList}`;
+  } else if (userValue < randomNumber) {
+    comment.innerText = `Way too Low\nYou Have Entered: ${userEntryList}`;
+  } else {
+    comment.innerText = "Enter a valid value";
   }
-};
-
-start_game.addEventListener("click", func);
+});
