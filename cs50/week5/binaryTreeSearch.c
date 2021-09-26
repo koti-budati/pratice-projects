c00ad62c6387eb300975fc07e0461d728aeafc30
koti-budati <koti.budati1994@outlook.com>
2021-09-26 14:32:17 +0530

    updated cs50

---
+++ cs50/week5/binaryTreeSearch.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+   int number;
+   struct node *left;
+   struct node *right; 
+} node;
+
+int main(void){
+    printf("hello\n");
+}
