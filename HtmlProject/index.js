151306219af093404e5761ad1c3e152270e2c56c
koti-budati <koti.budati1994@outlook.com>
2021-10-04 13:59:02 +0530

    removed unnecessary files

--- HtmlProject/index.js
+++ HtmlProject/index.js
@@ -1,2 +1,14 @@
+function render(tag, text, func) {
+  console.log(func);
+  if (func == undefined) {
+    const k = document.createElement(tag);
+    const content = document.createTextNode(text);
+    k.appendChild(content);
+    const body = document.getElementById("root");
+    document.body.insertBefore(k, body);
+  }
+}
 
-
+render("h1", "hello");
+render("h1", "there");
+render("h1", "world");
